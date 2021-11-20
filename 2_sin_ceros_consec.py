b14ed297678e5677c141f8714a122654c2dcbb07
okadath <vintaw.01@gmail.com>
2021-11-19 18:30:43 -0600

    ya limita a k ceros en la cadena

--- 2_sin_ceros_consec.py
+++ 2_sin_ceros_consec.py
@@ -11,6 +11,8 @@ def recursion(n,k,cadena,stri):
 	global array
 	if n<0:
 		return 1
+	if k<0:
+		return 1
 	if n==0 :#and k==0:
 		# cadena=cadena+"1"
 		# return 1-
@@ -18,22 +20,30 @@ def recursion(n,k,cadena,stri):
 			cadena=cadena+"1"
 		else:
 			cadena=cadena+"0"
-		array.append(cadena)
+			k=k-1
+		print("n="+str(n))
+		print("k="+str(k))
+		print(cadena)
+		if k>=0:
+			array.append(cadena)
+
 
 	else:
+		print(cadena)
 		# array.append(cadena)
 		# array.append(cadena)
 		if stri=="1":
 			cadena=cadena+"1"
 		else:
 			cadena=cadena+"0"
-		return recursion(n-1,k-1,cadena,"0"),recursion(n-1,k-1,cadena,"1")
+			k=k-1
+		return recursion(n-1,k,cadena,"0"),recursion(n-1,k,cadena,"1")
 		# else:
 			# cadena=cadena+"0"
 
 			# recursion(n-1,k-1,cadena,"0")
 
-recursion(int(n)-1,int(0),"","0")
-recursion(int(n)-1,int(0),"","1")
+recursion(int(n)-1,int(k),"","0")
+recursion(int(n)-1,int(k),"","1")
 print(array)
 print(len(array))
