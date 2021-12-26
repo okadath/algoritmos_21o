de3cc882889e75a607082ebb03bba449e29a1a4b
okadath <vintaw.01@gmail.com>
2021-12-25 19:14:19 -0600

    aumentando As explota

--- optativa.py
+++ optativa.py
@@ -16,31 +16,64 @@ cadena=b[0]
 
 
 
-
-
+cont=k
+letras=0
 maxim_rama=0
-lleno=0
+maxim_rama2=0
+
+i=0
+# while i<n:
+
 for j in range(0,n):
 	letras=0
-	cont=k
 	for i in range(j,n):
 		print(cadena[i])
-		print(j)
+		print(i)
+		print(cont)
+		print(letras)
+		print("   ")
+
+		# print(j)
 		if cadena[i]=="A":
 			cont=cont-1
 		else:
-			letras=letras+1
-		if i==n and cont>0:
-			lleno=1
+			letras=letras+1 
 		if letras>maxim_rama:
-				maxim_rama=letras
+			maxim_rama=letras
+				
 		if cont<0:
-			
-			break
-	if lleno:
-		break
+			# print("a")
+			letras=0
+			cont=k
+		i=i+1
 
+# cont=k
+# letras=0
+# # maxim_rama=0
+# maxim_rama2=0
+# i=n-1
+# while i>=0:
+# 	# pass
+# 	print(cadena[i])
+# 	print(i)
+# 	print(cont)
+# 	print("   ")
+# 	# print(j)
+# 	if cadena[i]=="A":
+# 		cont=cont-1
+# 	else:
+# 		letras=letras+1 
+# 	if letras>maxim_rama2:
+# 		maxim_rama2=letras
+			
+# 	if cont<0:
+# 		print("a")
+
+# 		letras=0
+# 		cont=k
+# 	i=i-1
 			# letras=letras+1
 print(maxim_rama)
+# print(maxim_rama2)
 
 # # animal
